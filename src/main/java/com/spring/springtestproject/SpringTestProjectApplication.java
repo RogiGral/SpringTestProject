32f2a7efcd56dbc8420bf658c9958624e6693717
RogiGral <igor.gralak99@gmail.com>
2021-07-15 00:18:07 +0200

    Initial commit

---
+++ src/main/java/com/spring/springtestproject/SpringTestProjectApplication.java
@@ -0,0 +1,21 @@
+package com.spring.springtestproject;
+
+import com.spring.springtestproject.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+public class SpringTestProjectApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringTestProjectApplication.class, args);
+    }
+
+
+
+}
---
+++ src/main/java/com/spring/springtestproject/student/StudentController.java
@@ -0,0 +1,27 @@
+package com.spring.springtestproject.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "app/v1/student")
+public class StudentController {
+    @GetMapping
+    public List<Student> hello()
+    {
+        return List.of(
+                new Student(
+                        1L,
+                        "Marian",
+                        "lol.a@gmail.com",
+                        LocalDate.of(1999, Month.JUNE,3),
+                        22
+                )
+        );
+    }
+}
